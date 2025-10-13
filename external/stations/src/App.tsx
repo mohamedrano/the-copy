24a59ec917e2df2782249195772f307ca4dada2e
mohamedrano <menot_for_hire@hotmail.com>
2025-10-13 21:22:49 +0300

    Scope stations router under base path

--- external/stations/src/App.tsx
+++ external/stations/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router as WouterRouter, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,12 +8,14 @@ import { LanguageProvider } from "@/contexts/LanguageContext";
 import HomePage from "@/pages/HomePage";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={HomePage} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/stations">
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
@@ -24,7 +26,7 @@ function App() {
         <ThemeProvider>
           <LanguageProvider>
             <Toaster />
-            <Router />
+            <AppRouter />
           </LanguageProvider>
         </ThemeProvider>
       </TooltipProvider>
